f66eb6f2bf749f5e1bb6b00c40a87a73855329c1
ranay <ranayetgin@hotmail.com>
2021-08-24 00:53:58 +0300

    unused method deleted

--- src/main/java/com/example/migros/demo/converter/StoreConverter.java
+++ src/main/java/com/example/migros/demo/converter/StoreConverter.java
@@ -11,22 +11,6 @@ import java.util.List;
 
 public class StoreConverter {
 
-
-    public List<StoresDto> convertJsonToStore() {
-        List<StoresDto> storeList = new ArrayList<>();
-        String jsonString;
-        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("YourJsonFile")) {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode = mapper.readValue(in, JsonNode.class);
-            jsonString = mapper.writeValueAsString(jsonNode);
-            System.out.println(jsonString);
-            storeList = Arrays.asList(mapper.readValue(jsonString, StoresDto[].class));
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return storeList;
-    }
     private StoreConverter() {
         //Should not be instantiated.
     }
